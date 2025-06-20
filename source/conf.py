75a672840b2fcef443512c845f054d40df53b80c
osada-yum <osada.yum@gmail.com>
2025-06-21 08:44:57 +0900

    Add sphinx.ext.githubpages for Github Pages.

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,7 @@ author = 'osada-yum'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.githubpages"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
